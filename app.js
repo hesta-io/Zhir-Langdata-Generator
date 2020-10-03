8a17b939d78904a56d64b6cd81a165e7be454a4a
aram rafeq <aramrafeq2@gmail.com>
2020-10-03 22:32:12 +0300

    unigram freq are now sorted

--- app.js
+++ app.js
@@ -2,55 +2,58 @@ import _ from 'https://jspm.dev/lodash@4';
 const inputFile = "./data/in/corpse.txt";
 const outputFile = "./data/out/corpse.txt";
 
+
 /* Fix line widths in words */
 console.log("Fixing Word Lengths 🕒");
 const inputText = await Deno.readTextFile(inputFile);
 const fixed = fixLineLength(inputText);
 await Deno.writeTextFile(outputFile, fixed);
-console.log("Sentence length fixed out/corpse.txt ✔️");
+console.log("Sentence length fixed ./data/out/corpse.txt ✔️");
 /* --------------------------------------- */
 
 /* Generating unigram frequnecies */
-//  bellow array should be sorted also
 console.log("Generating unigram frequencies 🕒");
-const unigramFreqMapIndexes: string[] = [];
-const unigramFreqMapValues: number[] = [];
+const unigramFreqObject = {};
 const spacesRemoved = fixed.trim().replace( / +/g, "" ).replace( /\n/g, "" );
 let unigramFreqString = "";
 console.log(`Processing ${spacesRemoved.length} characters 🕒`);
 for(let c of spacesRemoved){
-    const charIndex = unigramFreqMapIndexes.indexOf(c);
-    if( charIndex < 0){
-        unigramFreqMapIndexes.push(c);
-        unigramFreqMapValues.push(1);
-    } else{
-        unigramFreqMapValues[charIndex] += 1;
+    if(!unigramFreqObject[c]){
+        unigramFreqObject[c] = 1;
+    }else{
+        unigramFreqObject[c] +=1;
     }
 }
-unigramFreqMapIndexes.forEach((char)=>{
-    const charIndex = unigramFreqMapIndexes.indexOf(char);
-    unigramFreqString+= `${char} ${unigramFreqMapValues[charIndex]}\n`
+const unigramFreqArray = [];
+for (let key in unigramFreqObject) {
+    // deno-lint-ignore no-prototype-builtins
+    if (unigramFreqObject.hasOwnProperty(key)) {
+        unigramFreqArray.push({
+            char: key,
+            freq: unigramFreqObject[key],
+        })
+    }
+}
+const sortedUnigramFreqArray = _.orderBy(unigramFreqArray, ['freq'],['desc']);
+sortedUnigramFreqArray.forEach((r)=>{
+    unigramFreqString+= `${r.char} ${r.freq}\n`
 })
 await Deno.writeTextFile("./data/out/corpse.unigram_freqs", unigramFreqString);
 console.log("unigram frequencies saved out/corpse.unigram_freqs 🕒");
 /* --------------------------------------- */
+
 /* Generate Wordlist */
 console.log("Generating wordlist 🕒");
 const words = fixed.trim().replace( /\n+/g, " " ).split(" ");
 const uniqWords = _.uniq(words);
-let wordlistStr = "";
-uniqWords.forEach((word: string)=>{
-    wordlistStr+= word+"\n";
-});
+let wordlistStr = uniqWords.join("\n");
 await Deno.writeTextFile("./data/out/corpse.wordlist", wordlistStr);
-
-console.log("Wordlist saved out/corpse.wordlist 🕒");
-
+console.log("Wordlist saved ./data/out/corpse.wordlist 🕒");
 /* --------------------------------------- */
 
 console.log("All Done ✔️");
 
-function fixLineLength(input :string){
+function fixLineLength(input){
     const words =  input.trim().replace( /\n/g, " " ).split(" ");
     let appendedWords =0;
     let maxSentenceInWords = 7;
@@ -67,6 +70,6 @@ function fixLineLength(input :string){
     });
     return fixed;
 }
-function randomIntFromInterval(min :number, max :number) { // min and max included 
+function randomIntFromInterval(min, max) { // min and max included 
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
